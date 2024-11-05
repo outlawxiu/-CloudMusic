657a875a0f7471115703473f941a694a8784e59f
张典祥 <2848929891@qq.com>
2024-11-05 22:42:21 +0800

    firstSubmit

---
+++ my-vue3-project/src/components/top.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script setup lang="ts">
+import { reactive, ref } from "vue";
+import { RouterLink, RouterView } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
+const route = useRoute();
+const router = useRouter();
+</script>
+
+<style lang="scss" scoped></style>
